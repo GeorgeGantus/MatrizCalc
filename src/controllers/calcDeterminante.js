0d488d8978763d8271d0d3063c00b66090c9ea89
Victor Amaral <victoramaral@usp.br>
2020-12-21 14:40:40 -0300

    final(?)

--- src/controllers/calcDeterminante.js
+++ src/controllers/calcDeterminante.js
@@ -2,45 +2,47 @@ module.exports = {
     get: async (req, res) => {
 
         arrays = req.body.matriz
-        det = calcMatrix(arrays)
-        return res.json({ det: det })
-    },
+        var det = calcMatrix(arrays)
 
+        return res.json({ det })
+    },
 }
 
 function calcMatrix(matrix) {
-    for (i = 0; i < matrix[0].length - 1; i++) {
+    matrixLength = matrix[0].length
+    for (i = 0; i < matrixLength - 1; i++) {
 
-        for (j = i + 1; j < matrix[0].length; j++) {
+        for (j = i + 1; j < matrixLength; j++) {
 
             // if the pivot is 0, swap with non-zero pivot line
             if (matrix[i][i] == 0) {
                 var n = i + 1
                 while (matrix[n][i] == 0)
                     n++;
-                swapLine(matrix, i, n)
+                swapLine(matrix, matrixLength, i, n)
             }
 
             var kappa = matrix[j][i] / matrix[i][i]
 
-            for (k = i; k < matrix[0].length; k++) {
+            for (k = i; k < matrixLength; k++) {
                 matrix[j][k] = matrix[j][k] - kappa * matrix[i][k]
             }
 
         }
-        console.log(matrix)
     }
 
 
     // multiply diagonal
     var det = 1
-    for (i = 0; i < matrix[0].length; i++) {
+    for (i = 0; i < matrixLength; i++) {
+
         det *= matrix[i][i]
     }
+
     return det
 }
-function swapLine(matrix, i, n) {
-    for (k = 0; k < matrix[0].length; k++) {
+function swapLine(matrix, matrixLength, i, n) {
+    for (k = 0; k < matrixLength; k++) {
         var aux = -matrix[i][k]
         matrix[i][k] = matrix[n][k]
         matrix[n][k] = aux
